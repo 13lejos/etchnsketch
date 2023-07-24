781d047469023bf98db79ea3ef3e364f15dcc95f
Joseph Le <13lejoseph@gmail.com>
2023-07-24 09:37:02 -0700

    Script adding 16x16 boxes into our container

---
+++ script.js
@@ -0,0 +1,7 @@
+createGrid = function(){
+    const drawGrid = document.getElementById('etchContainer');
+    let drawnGrid = document.createElement('div')
+    drawnGrid.classList.add('cube')
+    drawGrid.appendChild(drawnGrid)
+    return('hello world')
+}
