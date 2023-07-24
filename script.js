acce8057e80494b4dcab32ade1ab272be5a310a2
Joseph Le <13lejoseph@gmail.com>
2023-07-24 14:09:00 -0700

    Add value input, grid, and drawing on grid

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
+const cube = document.querySelector('cube');
+let inputSize;
+
+
+
 createGrid = function(){
+    document.querySelectorAll('.cube').forEach(el => el.remove());
+    inputSize = document.getElementById('size').value;
+
+    let size = 512/inputSize;
+    let grid = (512/size)**2
+    if(inputSize <2 || inputSize >100){
+        console.log("sorry can't do that")
+    }
+    else{
+        for (step = 0; step < grid; step++){
     const drawGrid = document.getElementById('etchContainer');
     let drawnGrid = document.createElement('div')
     drawnGrid.classList.add('cube')
+    drawnGrid.style.width = size+'px'
+    drawnGrid.style.height = size+'px'
     drawGrid.appendChild(drawnGrid)
-    return('hello world')
+    drawnGrid.addEventListener('mouseover', () =>{
+        drawnGrid.style.backgroundColor = 'black'
+    })}}
+}
+
+
+returnInputSize = function(){
+    inputSize = document.getElementById('size').value;
 }
